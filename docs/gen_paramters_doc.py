f4e7b585c2bc1e394b76f26aa2d61b90107647bc
Andrew Barna <abarna@gmail.com>
2015-11-18 11:30:06 -0800

    collect param object which only differ in the unit field

--- docs/gen_paramters_doc.py
+++ docs/gen_paramters_doc.py
@@ -1,7 +1,7 @@
 import json
 
 with open("metadata/parameters.json", 'r') as f:
-    params = json.load(f)
+    param_list = json.load(f)
 
 output = '''.. _Parameters:
 
@@ -49,14 +49,34 @@ This section was generated automatically from a
   :columns: 3
 
 '''
-for param in params['parameters']:
+import itertools
+params = []
+groups = itertools.groupby(param_list["parameters"], key=lambda x: x["whp_name"])
+for group in groups:
+    first = next(group[1])
+    params.append(first)
+    for alt in group[1]:
+        # we only care about the human readable stuff for this
+        tests = [
+                alt["description"] == first["description"],
+                alt["note"] == first["note"],
+                alt["warning"] == first["warning"]
+                ]
+        if all(tests):
+            try:
+                first["alt_units"].append(alt["whp_unit"])
+            except KeyError:
+                first["alt_units"] = [alt["whp_unit"]]
+        else:
+            params.append(alt)
+for param in params:
     unit = param['whp_unit']
     if unit == '':
         output += "  * {0}_\n".format(param["whp_name"])
     else:
         output += "  * `{0} ({1})`_\n".format(param["whp_name"], param['whp_unit'])
 
-for param in params['parameters']:
+for param in params:
     unit = param['whp_unit']
     if unit == '':
         output += '''
@@ -79,6 +99,11 @@ for param in params['parameters']:
     data_label = "Data Type"
     quality_label = "{}_FLAG_W Definitions".format(param['whp_name'])
 
+    try:
+        alternate_units = ",".join(param["alt_units"])
+    except KeyError:
+        alternate_units = ""
+
     if quality_flags == "woce_ctd":
         quality_flags = ":ref:`CTD Quality Codes`"
     if quality_flags == "woce_discrete":
@@ -87,7 +112,7 @@ for param in params['parameters']:
         quality_flags = ":ref:`Bottle Quality Codes`"
 
     first_col = max(len(units_label), len(data_label), len(quality_label))
-    second_col = max(len(unit), len(data_type), len(quality_flags))
+    second_col = max(len(unit), len(data_type), len(quality_flags), len(alternate_units))
 
 
 
@@ -96,6 +121,8 @@ for param in params['parameters']:
     output += data_label.ljust(first_col) + ' ' + data_type + '\n'
     if quality_flags != "None":
         output += quality_label.ljust(first_col) + ' ' + quality_flags + '\n'
+    if "alt_units" in param:
+        output += "Alternate Units".ljust(first_col) + ' ' + alternate_units + '\n'
     output += "=" * first_col + ' ' + "=" * second_col + '\n'
 
     output += """
