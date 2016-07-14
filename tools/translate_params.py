fe3f860a135bc04fa960b1b52476589281790ba3
Andrew Barna <abarna@gmail.com>
2016-07-14 14:43:16 -0700

    create new params json format and schema

---
+++ tools/translate_params.py
@@ -0,0 +1,50 @@
+import json
+
+with open("meta/old_params.json") as f:
+    parameters = json.load(f)
+
+new_list = []
+for param in parameters:
+
+    remove = ("udunits", "whp_tier", "string_format")
+    remove_if_empty = ("note", "warning", "description", )
+    remove_if_null = ("cf_name", "numeric_max", "numeric_min")
+
+    null_if_empty = ("whp_unit", )
+
+    string_format = param["string_format"]
+
+    for prop in remove:
+        del param[prop]
+
+    for prop in remove_if_empty:
+        if len(param[prop].strip()) == 0:
+            del param[prop]
+
+    for prop in remove_if_null:
+        if param[prop] is None:
+            del param[prop]
+
+    for prop in null_if_empty:
+        if len(param[prop].strip()) == 0:
+            param[prop] = None
+
+
+    if param["data_type"] == "string":
+        param["field_width"] = int(string_format[1:-1])
+
+    if param["data_type"] == "integer":
+        param["field_width"] = int(string_format[1:-1])
+
+    if param["data_type"] == "decimal":
+        width, precision = string_format[1:-1].split(".")
+
+        param["field_width"] = int(width)
+        param["precision"] = int(precision)
+
+
+
+    new_list.append(param)
+
+with open("meta/parameters.json", 'w') as f:
+    json.dump(new_list, f, indent=2, sort_keys=True)
