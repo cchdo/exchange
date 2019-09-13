419ffb3db0d38e4ab2e6e88682c1e5bd04b51983
Andrew Barna <abarna@gmail.com>
2019-09-13 12:56:42 -1000

    add aliases for exchnage param names

--- test/testParameters.py
+++ test/testParameters.py
@@ -11,6 +11,11 @@ json_path = os.path.normpath(json_path)
 schema_path = os.path.join(this_dir, "..", "meta/parameters.schema.json")
 schema_path = os.path.normpath(schema_path)
 
+alias_json_path = os.path.join(this_dir, "..", "meta/aliases.json")
+alias_json_path = os.path.normpath(alias_json_path)
+alias_schema_path = os.path.join(this_dir, "..", "meta/aliases.schema.json")
+alias_schema_path = os.path.normpath(alias_schema_path)
+
 class TestParametersJSON(unittest.TestCase):
     def setUp(self):
         with open(json_path, 'r') as f:
@@ -24,8 +29,22 @@ class TestParametersJSON(unittest.TestCase):
             except ValueError:
                 self.fail(msg="The paramters schema is not valid JSON")
 
+        with open(alias_json_path, 'r') as f:
+            try:
+                self.aliases = json.load(f)
+            except ValueError:
+                self.fail(msg="The alias file is not valid JSON")
+        with open(alias_schema_path, 'r') as f:
+            try:
+                self.alias_schema = json.load(f)
+            except ValueError:
+                self.fail(msg="The alias schema is not valid JSON")
+
     def testValidateSchema(self):
         validate(self.parameters, self.schema)
+
+    def testValidateAliasSchema(self):
+        validate(self.aliases, self.alias_schema)
 
     def testUniqueParameters(self):
         pairs = []
@@ -34,6 +53,12 @@ class TestParametersJSON(unittest.TestCase):
 
         self.assertEqual(len(pairs), len(set(pairs)), msg="Duplicate param "\
         "name/unit pairs")
+
+    def testAliasMapsToCanonical(self):
+        pairs = {((param['whp_name'], param['whp_unit'])) for param in self.parameters}
+        for alias in self.aliases:
+            self.assertIn((alias['canonical_name'], alias["canonical_unit"]), pairs)
+
 
 
 
