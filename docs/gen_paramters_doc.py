1210daf5aa1773d31f5d42fc5a852d9ff0b53c44
Andrew Barna <abarna@gmail.com>
2022-03-22 19:54:18 +0000

    Add nc_var name to the params list

--- docs/gen_paramters_doc.py
+++ docs/gen_paramters_doc.py
@@ -12,6 +12,10 @@ flag_refs = {
 parameters = WHPNames.legacy_json
 schema = WHPNames.legacy_json_schema
 
+params_sorted = list(
+            dict.fromkeys(sorted(WHPNames.values(), key=lambda x: x.rank)).keys()
+        )
+
 with open("parameters.json", "w") as f:
     json.dump(parameters, f, indent=2)
 with open("parameters.schema.json", "w") as f:
@@ -20,38 +24,43 @@ with open("parameters.schema.json", "w") as f:
 
 ## group the params that only are only different by units into a single param
 params = []
-groups = itertools.groupby(parameters, key=lambda x: x["whp_name"])
+groups = itertools.groupby(params_sorted, key=lambda x: getattr(x, "whp_name"))
 for group in groups:
     first = next(group[1])
     params.append(first)
     for alt in group[1]:
         # we only care about the human readable stuff for this
         tests = [
-                alt.get("description") == first.get("description"),
-                alt.get("note") == first.get("note"),
-                alt.get("warning") == first.get("warning"),
+                getattr(alt, "description") == getattr(first, "description"),
+                getattr(alt, "note") == getattr(first, "note"),
+                getattr(alt, "warning") == getattr(first, "warning"),
                 ]
         if all(tests):
+            print(alt)
             try:
-                first["alt_units"].append(str(alt["whp_unit"]))
-                first["alt_cf_units"].append(str(alt.get("cf_unit",)))
-                first["alt_cf_names"].append(str(alt.get("cf_name",)))
+                first.__dict__["alt_units"].append(str(alt.whp_unit))
+                first.__dict__["alt_cf_units"].append(str(alt.cf_unit))
+                first.__dict__["alt_cf_names"].append(str(alt.cf_name))
+                first.__dict__["alt_nc_name"].append(str(alt.nc_name))
             except KeyError:
-                first["alt_units"] = [str(alt["whp_unit"])]
-                first["alt_cf_units"] = [str(alt.get("cf_unit", ))]
-                first["alt_cf_names"] = [str(alt.get("cf_name", ))]
+                first.__dict__["alt_units"] = [str(alt.whp_unit)]
+                first.__dict__["alt_cf_units"] = [str(alt.cf_unit)]
+                first.__dict__["alt_cf_names"] = [str(alt.cf_name)]
+                first.__dict__["alt_nc_name"] = [str(alt.nc_name)]
         else:
             params.append(alt)
 
 def gen_cf_table(param):
-    whp_units = [param.get("whp_unit"), *param.get("alt_units", [])]
-    cf_units = [param.get("cf_unit"), *param.get("alt_cf_units", [])]
-    cf_names = [param.get("cf_name"), *param.get("alt_cf_names", [])]
+    whp_units = [param.whp_unit, *param.__dict__.get("alt_units", [])]
+    cf_units = [param.cf_unit, *param.__dict__.get("alt_cf_units", [])]
+    cf_names = [param.cf_name, *param.__dict__.get("alt_cf_names", [])]
+    cf_vars = [param.nc_name, *param.__dict__.get("alt_nc_name", [])]
 
     rows = []
-    for whp, unit, name in zip(whp_units, cf_units, cf_names):
+    for nc_var, whp, unit, name in zip(cf_vars, whp_units, cf_units, cf_names):
         rows.append(dedent(f"""\
-        * - ``{whp}``
+        * - ``{nc_var}``
+          - ``{whp}``
           - ``{unit}``
           - ``{name}``\
         """))
@@ -60,7 +69,8 @@ def gen_cf_table(param):
         .. list-table::
             :header-rows: 1
 
-            * - ``whp_unit``
+            * - ``nc_var`` name
+              - ``whp_unit``
               - ``units``
               - ``standard_name``\
         """)
@@ -88,50 +98,50 @@ parameters json.
 
 # Make the 3 column list of params at the top of the page
     for param in params:
-        name = param["whp_name"]
+        name = param.whp_name
         f.write(f"  * `{name}`_\n")
 
     for param in params:
-        name = param["whp_name"]
-        unit = param.get('whp_unit')
+        name = param.whp_name
+        unit = param.whp_unit
 
         ## make the ref and title
         f.write(f"\n.. _{name}:\n\n{name}\n")
 
         ## Underline that title
-        f.write("^" * len(param["whp_name"]) + '\n\n')
-        data_type = param['data_type']
-        quality_flags = str(param['flag_w'])
+        f.write("^" * len(param.whp_name) + '\n\n')
+        data_type = param.dtype
+        quality_flags = param.flag_w
 
         f.write(f":Units:\n  * {unit}\n")
-        if "alt_units" in param:
-            for alt_unit in param["alt_units"]:
+        if "alt_units" in param.__dict__:
+            for alt_unit in param.__dict__["alt_units"]:
                 f.write(f"  * {alt_unit}\n")
 
         f.write(f":Data Type: {data_type}\n")
-        if quality_flags != "None":
+        if quality_flags != "no_flags":
             flag_ref = flag_refs[quality_flags]
-            f.write(f":{param['whp_name']}_FLAG_W Definitions: {flag_ref}\n")
+            f.write(f":{param.whp_name}_FLAG_W Definitions: {flag_ref}\n")
 
-        if "error_name" in param:
-            f.write(f":Error Column Label: {param.get('error_name', '')}\n")
+        if param.error_name is not None:
+            f.write(f":Error Column Label: {param.error_name}\n")
 
         f.write(f"\n:CF/netCDF Attributes:\n{indent(gen_cf_table(param), '  ')}\n")
 
-        f.write(f"\n{param.get('description', '')}\n")
+        f.write(f"\n{param.description}\n")
 
         if quality_flags == "None":
             f.write((f"\n.. warning::\n"
                        f"  ``{name}`` does not have woce quality codes."
                        f"  ``{name}_FLAG_W`` should not appear in data file"
                        f"  :ref:`parameter and unit lines`.\n"))
-        if param.get('note'):
+        if param.note is not None:
             f.write("\n.. note::\n")
-            for line in param['note'].split("\n"):
+            for line in param.note.split("\n"):
                 f.write(f"  {line}\n")
-        if param.get('warning'):
+        if param.warning is not None:
             f.write("\n.. warning::\n")
-            for line in param['warning'].split("\n"):
+            for line in param.warning.split("\n"):
                 f.write(f"  {line}\n")
 
         f.write("\n.. raw:: html\n\n  <hr />\n")
